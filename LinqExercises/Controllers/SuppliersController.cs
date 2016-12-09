5672504c0c5a95ef96e16532241e9ca5b5576593
Alex <amcel1288@gmail.com>
2016-12-08 16:18:05 -0800

    Finished exercises

--- LinqExercises/Controllers/SuppliersController.cs
+++ LinqExercises/Controllers/SuppliersController.cs
@@ -19,14 +19,22 @@ namespace LinqExercises.Controllers
         [HttpGet, Route("api/suppliers/salesAndMarketing"), ResponseType(typeof(IQueryable<Supplier>))]
         public IHttpActionResult GetSalesAndMarketing()
         {
-            throw new NotImplementedException("Write a query to return all Suppliers that are marketing managers or sales representatives that have a fax number");
+            //throw new NotImplementedException("Write a query to return all Suppliers that are marketing managers or sales representatives that have a fax number");
+            var resultSet = _db.Suppliers
+                .Where(s => s.ContactTitle == "Marketing Manager" ||  s.ContactTitle == "Sales Representative")
+                .Where(s => !s.Fax.Equals(null));
+            return Ok(resultSet);
         }
 
         //GET: api/suppliers/search
         [HttpGet, Route("api/suppliers/search"), ResponseType(typeof(IQueryable<Supplier>))]
         public IHttpActionResult SearchSuppliers(string term)
         {
-            throw new NotImplementedException("Write a query to return all Suppliers containing the 'term' variable in their address. The list should ordered alphabetically by company name.");
+            //throw new NotImplementedException("Write a query to return all Suppliers containing the 'term' variable in their address. The list should ordered alphabetically by company name.");
+            var resultSet = _db.Suppliers
+                .Where(s => s.Address.Contains(term))
+                .OrderBy(s => s.CompanyName);
+            return Ok(resultSet);
         }
 
         protected override void Dispose(bool disposing)
